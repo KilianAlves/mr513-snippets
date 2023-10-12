d77476848663350af016e0a6565cdb1e84727bcb
KilianAlves <alveskilian@gmail.com>
2023-10-12 11:43:48 +0200

    ajout de expressAsyncHandler

--- src/snippets/snippets.routes.ts
+++ src/snippets/snippets.routes.ts
@@ -1,10 +1,13 @@
 import express, { Router } from 'express';
 import { SnippetsController } from './snippets.controller'
 import session from 'express-session';
+import expressAsyncHandler from 'express-async-handler';
 
 const router: Router = express.Router();
 
-router.get('/', SnippetsController.list);
+router.get('/', 
+    expressAsyncHandler(SnippetsController.list)
+);
 
 export = router;
 
