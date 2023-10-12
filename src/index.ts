0fae3a319bcda93a42c4bf8a993310cda1783132
KilianAlves <alveskilian@gmail.com>
2023-10-12 11:40:08 +0200

    Gestionnaire d'erreurs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import session from 'express-session';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 var routes = require('./snippets/snippets.routes');
 
@@ -21,6 +22,11 @@ declare module "express-session" {
     interface SessionData {
     }
 }
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(`ERREUR : ${err.message}`);
+    res.render('error', { err });
+});
 
 app.listen(port, () => {
     console.log(`Server local démarré : http://localhost:${port}`);
--- src/snippets/snippets.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SnippetsController = void 0;
-class SnippetsController {
-    static list(req, res, next) {
-        throw new Error("Ceci est un message d'erreur");
-    }
-}
-exports.SnippetsController = SnippetsController;
