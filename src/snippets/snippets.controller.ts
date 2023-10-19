335deb899d6ed64c0a0806cba02058e7e65bd051
Erwann Maton <erwann.mutant@gmail.com>
2023-10-19 10:53:03 +0200

    Affichage des logos terminés

--- src/snippets/snippets.controller.ts
+++ src/snippets/snippets.controller.ts
@@ -2,10 +2,15 @@ import { NextFunction, Request, Response } from 'express';
 import { prisma } from '../services/prima';
 
 export class SnippetsController {
-    
+
     static async list(req: Request, res: Response, next: NextFunction): Promise<void> {
-        
-        const snippets = await prisma.snippet.findMany();
+
+        const snippets = await prisma.snippet.findMany(
+            {
+                include: {
+                    language: true,
+                }
+            });
         res.render('snippets/snippets_list', {'snippets': snippets });
     }
 }
