860cd6ab9e742479b6eb611f6de6ba4c4803fdf3
Erwann Maton <erwann.mutant@gmail.com>
2023-10-19 10:40:10 +0200

    Modification du schéma terminé

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,38 @@
 import { PrismaClient } from '@prisma/client';
-    
+
 const prisma = new PrismaClient();
-    
+
 async function main() {
+
+    await prisma.language.create({
+        data: {
+            name: 'C',
+            htmlClass: 'language-c',
+            logo: 'devicon-c-plain'
+        }
+    });
+
+    await prisma.language.create({
+        data: {
+            name: 'HTML',
+            htmlClass: 'language-html',
+            logo: 'devicon-html5-plain'
+        }
+    });
+
     await prisma.snippet.create({
         data: {
             title: 'Hello World',
             code:
-`main()
-{
-    printf("hello, world\\n");
-}`,
+                `main()
+                {
+                    printf("hello, world\\n");
+                }`,
             description: 'Code original publié dans "The C Programming Language" de Brian Kernighan et Dennis Ritchie.',
             creationDate: new Date(2023, 4, 8, 9, 12, 36),
+            language: {
+                connect : {name : 'C'}
+            }
         }
     });
 
@@ -21,7 +41,10 @@ async function main() {
             title: 'Il faut protéger ses chaînes de caractères',
             code: '<script>window.alert("Injection !")</script>',
             creationDate: new Date(2023, 3, 4, 5, 6, 7),
-            description: 'Dans le template EJS, observez le comportement de la page en utilisant successivement les balises <%- et <%=pour injecter les données.', 
+            description: 'Dans le template EJS, observez le comportement de la page en utilisant successivement les balises <%- et <%=pour injecter les données.',
+            language: {
+                connect : {name : 'HTML'}
+            }
         }
     });
 
@@ -30,11 +53,13 @@ async function main() {
             title: 'Bouton de téléchargement',
             code: ' <a href="/images/myw3schoolsimage.jpg" download> ',
             creationDate: new Date(2023, 3, 4, 5, 6, 7),
-            description: 'Telecharge un fichier quand on clique sur le lien.', 
+            description: 'Telecharge un fichier quand on clique sur le lien.',
+            language: {
+                connect : {name : 'HTML'}
+            }
         }
     });
 }
-
 main().catch(e => {
     console.error(e);
     process.exit(1);
